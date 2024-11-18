0c784595081434b493b9876a13db68250be3d929
Maralasai <saikrupasai11@gmail.com>
2024-11-18 20:28:16 +0530

    Initial commit

---
+++ src/components/UserList.js
@@ -0,0 +1,42 @@
+// src/components/UserTable.js
+import React from "react";
+
+const UserList = ({ users, onEdit, onDelete }) => {
+  return (
+    <div> <h1 style={{color:"red",fontSize:"25px",textAlign:"center"}}>Registered Users</h1>
+    <table border="1" style={{ width: "100%", textAlign: "left" }}>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Date of Birth</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.length > 0 ? (
+          users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.dob}</td>
+              <td>
+                <button className="edit" onClick={() => onEdit(user)}>Edit</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <button className="delete" onClick={() => onDelete(user.id)}>Delete</button>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan="4" style={{ textAlign: "center" }}>
+              No users available
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+    </div>
+  );
+};
+
+export default UserList;
